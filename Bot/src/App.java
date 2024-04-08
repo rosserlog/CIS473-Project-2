d7088fbec178f121374a88635a777f0f0801a573
rosserl <rosserl@mail.gvsu.edu>
2024-04-08 17:31:35 -0400

    getting closer this is so annoying

--- Bot/src/App.java
+++ Bot/src/App.java
@@ -1,5 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-    }
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        Thread.sleep(1000);
+        driver.quit();
+    
+    } 
 }
