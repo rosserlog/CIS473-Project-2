a05f000f0e69ac99d55072af33312fbc78296752
rosserlog <rosserl@mail.gvsu.edu>
2024-04-08 17:45:14 -0400

    Finally Got Selenium up and running only took 5 hours

--- Bot/src/App.java
+++ Bot/src/App.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\logan\\Repos\\CIS473-Project-2\\Bot\\src\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
